36597e0b3ebabfe6a831b0d5b98da50deb989f1b
Alexandra Deaconu <141563285+aledeaconu@users.noreply.github.com>
2024-05-23 11:29:22 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { HomeComponent } from './main-content/home/home.component';
+import { ProjectsComponent } from './main-content/projects/projects.component';
+import { ContactComponent } from './main-content/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    HeaderComponent,
+    MainContentComponent, 
+    HomeComponent, 
+    ProjectsComponent, 
+    ContactComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+   
   ],
   providers: [
     provideClientHydration()
